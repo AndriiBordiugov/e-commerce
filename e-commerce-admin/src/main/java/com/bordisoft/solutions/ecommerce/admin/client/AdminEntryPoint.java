7fda47bafcf5a31f4825e0961c6e30a7a4626faf
andrii.bordiugov <andrii.bordiugov@netcracker.com>
2016-04-18 14:16:13 +0300

    structure refactoring

---
+++ e-commerce-admin/src/main/java/com/bordisoft/solutions/ecommerce/admin/client/AdminEntryPoint.java
@@ -0,0 +1,11 @@
+package com.bordisoft.solutions.ecommerce.admin.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class AdminEntryPoint implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+
+    }
+}
