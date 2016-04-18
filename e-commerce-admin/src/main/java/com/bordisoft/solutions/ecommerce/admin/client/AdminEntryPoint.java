90264a33423bec8aec30235573f2adbe89956d14
andrii.bordiugov <andrii.bordiugov@netcracker.com>
2016-04-18 14:59:46 +0300

    structure refactoring

--- e-commerce-admin/src/main/java/com/bordisoft/solutions/ecommerce/admin/client/AdminEntryPoint.java
+++ e-commerce-admin/src/main/java/com/bordisoft/solutions/ecommerce/admin/client/AdminEntryPoint.java
@@ -1,11 +1,47 @@
 package com.bordisoft.solutions.ecommerce.admin.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.*;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.*;
+
+import java.util.ArrayList;
 
 public class AdminEntryPoint implements EntryPoint {
 
+    private VerticalPanel mainPanel = new VerticalPanel();
+    private FlexTable stocksFlexTable = new FlexTable();
+    private HorizontalPanel addPanel = new HorizontalPanel();
+    private TextBox newSymbolTextBox = new TextBox();
+    private Button addStockButton = new Button("Click");
+    private Label lastUpdatedLabel = new Label();
+    private Label errorMsgLabel = new Label();
+
+
     @Override
     public void onModuleLoad() {
+        stocksFlexTable.setText(0,0, "Symbol");
+        stocksFlexTable.setText(0,1, "Price");
+        stocksFlexTable.setText(0, 2, "Change");
+        stocksFlexTable.setText(0, 3, "Remove");
 
+        stocksFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
+        stocksFlexTable.addStyleName("watchList");
+        stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
+        stocksFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
+        stocksFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
+        stocksFlexTable.setCellPadding(5);
+
+        addPanel.add(newSymbolTextBox);
+        addPanel.add(addStockButton);
+        addPanel.addStyleName("addPanel");
+
+        errorMsgLabel.setStyleName("errorMessage");
+        errorMsgLabel.setVisible(false);
+
+        mainPanel.add(stocksFlexTable);
+        mainPanel.add(addPanel);
+        mainPanel.add(lastUpdatedLabel);
     }
 }
